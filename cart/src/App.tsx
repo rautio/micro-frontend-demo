bd6988ace00b83cada2e036cb75439fccdf2e3cf
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2022-05-28 09:31:39 -0700

    Fix cart standalone page crashing on checkout panel open

--- cart/src/App.tsx
+++ cart/src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import Cart from "./components/CartButton";
 
 export const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <h1>Cart</h1>
       <Cart />
-    </div>
+    </BrowserRouter>
   );
 };
 
